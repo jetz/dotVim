46cc3654778236b8c70b931d3fe4ed12356fe27a
jetz <jet.zheung@gmail.com>
2017-10-23 16:01:25 +0800

    Add vimcdoc as submodule

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 **Usage:**
 
-    1. git clone https://github.com/jetz/dotVim.git ~/.vim
+    1. git clone --recursive https://github.com/jetz/dotVim.git ~/.vim
     2. ln -s ~/.vim/.vimrc ~/.vimrc
-    3. ./init.sh
-    4. cd ~/.vim/plugged/YouCompleteMe && ./install.py
+    3. cd ~/.vim && ./init.sh && cd ..
--- init.sh
+++ init.sh
@@ -1,32 +1,14 @@
 #!/usr/bin/env bash
 
-DISTRO="Ubuntu"
-INSTALL="sudo apt install -y"
+function cecho() {
+    echo -e "\e[32m"$1"\e[0m"
+}
 
-if [[ -f /etc/redhat-release ]]; then
-    DISTRO="Redhat"
-    INSTALL="sudo yum install -y"
-fi
-
-
-echo ">>>>> Install YouCompleteMe Deps..."
-if [[ $DISTRO == "Ubuntu" ]]; then
-    $INSTALL build-essential cmake python-dev
-fi
-
-echo ">>>>> Install Autoformat Deps..."
-$INSTALL nodejs golang clang
-
-sudo pip install autopep8
-sudo npm install -g js-beautify
-
-
+cecho ">>>>> Install Plug..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo ">>>>> Install Plugins..."
+cecho ">>>>> Install Plugins..."
 vim +PlugInstall +qall
 
-echo ">>>>> Install Chinese doc..."
-cd ~/.vim && tar -xzf vimcdoc-1.9.0.tar.gz
-cd vimcdoc-1.9.0 && ./vimcdoc.sh -i
-cd .. && rm -rf vimcdoc-1.9.0
+cecho ">>>>> Install Chinese doc..."
+cd ~/.vim/vimcdoc && ./vimcdoc.sh -i
